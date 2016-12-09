88df994e037e6ee9930caf55b78d003f441e3d4c
Mark <grimelios@gmail.com>
2016-12-09 00:35:04 -0600

    First commit.

---
+++ LD37/MainGame.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LD37
+{
+	public class MainGame : Game
+	{
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+
+		public MainGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+		
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void UnloadContent()
+		{
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+		}
+	}
+}
