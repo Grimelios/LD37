83ed1ba8da854f980d16596e7d083756495afd5b
Mark <grimelios@gmail.com>
2016-12-12 16:26:12 -0600

    Correcting wire position.

--- LD37/Entities/SlidingDoor.cs
+++ LD37/Entities/SlidingDoor.cs
@@ -93,7 +93,7 @@ namespace LD37.Entities
 			}
 		}
 
-		public Vector2 WirePosition => Position - new Vector2(0, Constants.TileSize * 3);
+		public Vector2 WirePosition => Position - new Vector2(0, Constants.HalfTile * 7);
 
 		public override void Dispose()
 		{
